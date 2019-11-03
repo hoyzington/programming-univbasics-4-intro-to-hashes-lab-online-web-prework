b8f4a0fe35e434f5ae2bedec8ac07c0b6993da12
Scott Hoisington <hoyzington@gmail.com>
2019-11-03 05:01:15 +0000

    Automatically backed up by Learn

--- intro_to_ruby_hashes_lab.rb
+++ intro_to_ruby_hashes_lab.rb
@@ -29,9 +29,3 @@ def update_counting_hash(hash, key)
     hash[key] = 1
   end
 end
-
-if shipping_manifest["top hat"]
-  shipping_manifest["top hat"] += 1
-else
-  shipping_manifest["top hat"] = 1
-end
